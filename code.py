064ce891009993d506ac7c77aacb55adfdf8b60d
Rafa-ops-blip <ogirimarafah@gmail.com>
2024-11-20 11:58:32 +0100

    completed a function

---
+++ code.py
@@ -0,0 +1,7 @@
+print("This is a string")
+print("this is not a string")
+message = "Hello you"
+print(message)
+
+message = "Hello python"
+print(message.upper())
---
+++ file.py
@@ -0,0 +1,7 @@
+def student_name():
+  message = "This is a text"
+  return message
+
+print(student_name())
+s = student_name()
+print(s)
